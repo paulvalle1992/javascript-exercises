307943ad505a264e0bdfc5c4c013c6ff8b377648
Paul V <paulvalle1992@gmail.com>
2023-03-02 14:27:01 -0800

    looked up the different built-in functions with arrays to help finish my exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    const phrase = string.split("");
+      phrase.reverse();
+      let join = phrase.join("");
+    return join;
+    };
 // Do not edit below this line
 module.exports = reverseString;
