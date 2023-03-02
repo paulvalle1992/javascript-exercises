03a03075d0514b7a669ffd93d14bdbf7e7d255de
Paul V <paulvalle1992@gmail.com>
2023-03-02 12:56:30 -0800

    cheated on this one, saw i was going the right way and overcomplicated it, need better understanding of fundamentals operations

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let hey = "";
+    for (i = 0; i < num; i++) {
+      hey += string;
+    }
+    return hey;
 };
 
 // Do not edit below this line
