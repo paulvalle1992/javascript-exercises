3774cee78bdf451502b2502cb687abc600ac017f
Paul V <paulvalle1992@gmail.com>
2023-03-03 13:23:12 -0800

    obviously went my own route, i like my code a little better, it may be a bit longer but i think it is cleaner and more readable/understandable, should've commited more but key points : i made sure to check if number is integer and positive first then i chose whether first or second integer is higher the lower integer is where to begin

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(stInt, ndInt) {
+    let sum = 0
+    if (Number.isInteger(stInt) === false || Number.isInteger(ndInt) === false || stInt < 0 || ndInt < 0) {
+      return "ERROR";
+    } else if (stInt <= ndInt) {
+      for (let i = stInt; i <= ndInt; i++) {
+         sum += i;
+      } return sum;
+    } else if (stInt >= ndInt) {
+      for (let i = ndInt; i <= stInt; i++) {
+        sum += i;
+      } return sum;
+    } 
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
