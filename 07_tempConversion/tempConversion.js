0d216ba92ceddb6eb05deb322de035a186e97b35
Paul V <paulvalle1992@gmail.com>
2023-03-06 13:59:37 -0800

    passed all tests, used the equation and used tofixed to round down to the tenths while also using the number function to convert the string to an integer

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(degreeF) {
+    let celsius = ((degreeF - 32) * 5/9);
+    return Number(celsius.toFixed(1));
+  };
 
-const convertToFahrenheit = function() {
-};
+
+const convertToFahrenheit = function(degreeC) {
+  let fahrenheit = ((degreeC * 9/5) + 32);
+  return Number(fahrenheit.toFixed(1));
+  };
 
 // Do not edit below this line
 module.exports = {
