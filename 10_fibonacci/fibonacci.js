e603eaec086aed4614aa2381a149cfda74b63031
Paul V <paulvalle1992@gmail.com>
2023-05-05 14:37:14 -0700

    made the fibonacci sequence algo, used help on formatting and cleaned it up to replicate what was needed to pass the tests using an array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = (n) =>  {
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    const fib = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return Number(fib.splice(-1).toString())
 };
 
 // Do not edit below this line
