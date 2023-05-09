9872225cd1273ce23954566069a1f3592dbeb9fb
Paul V <paulvalle1992@gmail.com>
2023-05-08 17:05:28 -0700

    need a lot of work to do on this, need to understand better  why choosing this if then statement over the sorting if then for 1 -1, and other issues that i know i will run into if having to create another similar method of this.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest =  (array) => { 
+    return array.reduce((a, b) => {
+        const oldestPerson = getAge(a.yearOfBirth, a.yearOfDeath);
+        const currentPerson = getAge(b.yearOfBirth, b.yearOfDeath);
+        return oldestPerson > currentPerson ? a : b;
+  })
+};
+  
+const getAge = (birth, death) => {
+    if (!death) { 
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
